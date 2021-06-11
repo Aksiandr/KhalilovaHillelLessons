daf1a72c2ff760b4349e6b1393e42bc505503946
Aksiandr <Aksiandr@users.noreply.github.com>
2021-06-11 17:23:34 +0300

    Ninth h/w

--- ninth/src/GetBookByIndex.java
+++ ninth/src/GetBookByIndex.java
@@ -8,16 +8,17 @@ public class GetBookByIndex {
         ArrayList<Book> books = new ArrayList<>();
 
 
-        books.add(new Book(120, "Book 1", "soft", 250));
-        books.add(new Book(150, "Book 2", "hard", 300));
-        books.add(new Book(99, "Book 3", "soft", 130));
-        books.add(new Book(122, "Book 4", "hard", 235));
-        books.add(new Book(20, "Book 5", "soft", 99));
+        books.add(new Book(120, "Boys Book", "soft", 250));
+        books.add(new Book(150, "Girls Book", "hard", 300));
+        books.add(new Book(99, "Interesting Stories Book", "soft", 130));
+        books.add(new Book(122, "Book with games", "hard", 235));
+        books.add(new Book(20, "Parents Book", "soft", 99));
 
         for (int i = 0; i <= books.size(); i++) {
             try {
                 Book book = books.get(i);
-                System.out.println("Do you want to buy this book: " + book.getTitle());
+                System.out.println("Do you want to buy '" + book.getTitle() + "' with " +
+                        book.getCoverType() + " cover?");
             } catch (IndexOutOfBoundsException e) {
                 System.out.println("The book with index " + i + " is not present in the 'books' arrayList. \n");
             }
